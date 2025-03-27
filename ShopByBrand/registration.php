b076f2b389eac6a22855bdf1d0f49141b982ac17
Magecan <wuqun2005@hotmail.com>
2025-03-27 10:26:39 -0400

    Add files via upload

---
+++ ShopByBrand/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © Magecan, Inc. All rights reserved.
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Magecan_ShopByBrand',
+    __DIR__
+);
